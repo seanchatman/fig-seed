b1bf3153f9f2099142e5586c5d7ad3ae6781256b
Sean Chatman <xpointsh@gmail.com>
2014-09-01 22:30:17 -0700

    Version 0.1 feature complete.

--- fig-seed.py
+++ fig-seed.py
@@ -1,29 +1,83 @@
+#!/usr/bin/env python
 """ Fig Seed, plant your ideas.
 
 Usage:
   fig-seed.py list
-  fig-seed.py demo <name>
+  fig-seed.py up <template_name>
+  fig-seed.py [-v] init <target_directory>
+  fig-seed.py [-v] init <template_name> <target_directory>
 
+
+Arguments:
+  template_name        folder name in template directory
+  target_directory     where the template is copied.
+
+Options:
+  -h       --help
+  -v       verbose mode
+
+Example: fig-seed.py init /tmp/init
+         fig-seed.py init fig-flask /tmp/fig-flask
+         fig-seed.py up fig-flask
 """
 from docopt import docopt
+#from subprocess import call
 
-import os, sys, shutil
+import os
+import shutil
 
 __author__ = 'arby'
 
 
-def init(args):
-    print args
+def list_templates():
+    template_dir = os.getcwd() + '/template'
+    templates = os.listdir(template_dir)
+    for x in templates:
+        print('Template list:')
+        print(x)
+
+
+def export(name, dest, template_dir):
+    if os.path.isdir(dest):
+        if args['-v']:
+            print 'Found %s, removing.' % dest
+        shutil.rmtree(dest)
+
+    if args['-v']:
+        print 'Copying to %s.' % dest
+    template = template_dir + '/' + name
+    shutil.copytree(template, dest)
+    if args['-v']:
+        for root, dirs, files in os.walk(dest):
+            for dest_file in files:
+                print 'Copied %s.' % dest_file
+
+
+def init():
+    template_dir = os.getcwd() + '/template'
+
+    if args['<template_name>']:
+        if args['-v']:
+            print 'Creating %s.' % args['<template_name>']
+        export(args['<template_name>'], args['<target_directory>'], template_dir)
+    else:
+        if args['-v']:
+            print 'Creating %s.' % args['<template_name>']
+        export('init', args['<target_directory>'], template_dir)
+
+
+def up():
+    if args['-v']:
+        print 'Starting %s.' % args['<template_name>']
+    #call(['fig', ])
+
+
 
 if __name__ == '__main__':
-    args = docopt(__doc__, version='Naval Fate 2.0')
-    templateDir = os.getcwd() + '/template'
-    templates = os.listdir(templateDir)
+    args = docopt(__doc__, version='fig-seed 0.1')
 
     if args['list']:
-        print templates
+        list_templates()
 
-    if args['demo']:
-        print args['demo']
-        print args['<name>']
-        shutil.copytree(templateDir + '/' + args['<name>'], '/tmp/' + args['<name>'])
+    if args['init']:
+        init()
