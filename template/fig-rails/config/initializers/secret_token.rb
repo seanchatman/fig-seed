280b2e468212c88ed5a888c9cd2a2a5551a25806
Sean Chatman <xpointsh@gmail.com>
2014-09-05 01:54:26 -0400

    Commit from do.

---
+++ template/fig-rails/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Code::Application.config.secret_key_base = 'a74430ef50af35369465f57be18494384fbc951cee5d8e0686e6ed7a7c4f9f91b553591c8640e233e422fc8e5c7751ed304a9b77ab8b207859ce8dcc58247e56'
